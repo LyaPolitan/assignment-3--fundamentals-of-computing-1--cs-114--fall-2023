01f59188f99313b83d23dcb0dfbf9e909bb7e407
LyaPolitan <lyamperez04@gmail.com>
2023-11-15 16:47:48 -0500

    finished project
    i couldn't figure out a way to change the color of the values without do something really convoluted and confusing however everything should be workign as its supposed to

--- Matrix.java
+++ Matrix.java
@@ -1,18 +1,14 @@
 public class Matrix {
-  private void highlight(){
+//this class contains all the matrix equations
 
-
-
-
-
-
-  }
   public void emptyMatrix(int Xsize,int Ysize){
+    //creates a empty array
     int[][] table = new int[Ysize][Xsize];
     for (int row = 0; row < table.length; row++) {
       for (int col = 0; col < table[row].length; col++) {
       }
     }
+    //prints out an array of zeros
     for (int row = 0; row < table.length; row++) {
       for (int col = 0; col < table[row].length; col++) {
         System.out.print (table[row][col] + "\t");
@@ -20,18 +16,52 @@ public class Matrix {
     System.out.println();
     }
   }
+
+
+
   public void filledMatrix(int Xsize,int Ysize){
+    //Filling the matrix starting from zero to the (input x input)-1
     int[][] table = new int[Ysize][Xsize];
     for (int row = 0; row < table.length; row++) {
       for (int col = 0; col < table[row].length; col++) {
-        table[row][col] = row + col;
+        table[row][col] = (row * Ysize) + col;
     }
   }
+    //Printing out the matrix after filling with sumber sequentially
     for (int row = 0; row < table.length; row++) {
       for (int col = 0; col < table[row].length; col++) {
         System.out.print (table[row][col] + "\t");
     }
     System.out.println();
+    }
+  }
+
+
+
+  public void flippedMatrix(int Xsize, int Ysize) {
+    //creating a matrix and filling it with number sequentially
+    int[][] table = new int[Ysize][Xsize];
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+        table[row][col] = (row * Ysize) + col;
+      }
+    }
+    //Flipping values across the diagonal from bottom-left to top-right
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+        if (col < Ysize - row - 1) {
+          int temp = table[row][col];
+          table[row][col] = table[Ysize - col - 1][Xsize - row - 1];
+          table[Ysize - col - 1][Xsize - row - 1] = temp;
+        }
+      }
+    }
+    //Displaying the modified matrix
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+          System.out.print(table[row][col] + "\t");
+      }
+      System.out.println();
     }
   }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    //this part of the code only handles taking in the input from the user, As well as printing out everything user needs to see
     Matrix matrixes = new Matrix();
     Scanner scan = new Scanner(System.in);
     int size;
@@ -23,13 +24,12 @@ public class RunMatrix {
     System.out.print("printing matrix: ");
     System.out.println("");
     matrixes.filledMatrix(Xsize, Ysize);
-
-
-
-
-
-
-
+    System.out.println("");
+    System.out.println("Flipping matrix. . .matrix flipped");
+    System.out.println("");
+    System.out.print("Printing flipping matrix: ");
+    System.out.println("");
+    matrixes.flippedMatrix(Xsize, Ysize);
     scan.close();
   }
 }
