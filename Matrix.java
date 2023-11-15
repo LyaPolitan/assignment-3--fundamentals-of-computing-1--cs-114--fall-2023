5c9ab2e92adce78c73f65be5d062d0407c3bbe4f
LyaPolitan <lyamperez04@gmail.com>
2023-11-15 03:04:45 -0500

    empty matrix and filled matrix
    i added code that prints a empty matrix for the user to see, all in another file called matrix . i also added the same thing but the filled matrix

--- Matrix.java
+++ Matrix.java
@@ -1,12 +1,29 @@
 public class Matrix {
-  public void emptyMatrix(){
-    System.out.print
+  public void emptyMatrix(int Xsize,int Ysize){
+    int[][] table = new int[Ysize][Xsize];
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+      }
+    }
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+        System.out.print (table[row][col] + "\t");
+    }
+    System.out.println();
+    }
   }
-
-
-
-
-
-
-
+  public void filledMatrix(int Xsize,int Ysize){
+    int[][] table = new int[Ysize][Xsize];
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+        table[row][col] = row + col;
+    }
+  }
+    for (int row = 0; row < table.length; row++) {
+      for (int col = 0; col < table[row].length; col++) {
+        System.out.print (table[row][col] + "\t");
+    }
+    System.out.println();
+    }
+  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    RunMatrix matrixObject = new RunMatrix();
+    Matrix matrixes = new Matrix();
     Scanner scan = new Scanner(System.in);
-    int Size;
-
+    int size;
     System.out.print("Please enter the size of your matrix: ");
-    Size = scan.nextInt();
+    size = scan.nextInt();
+    int Xsize=size;
+    int Ysize=size;
     System.out.println();
-    System.out.print("Your matrix is "+ Size);
-    System.out.println(" x "+Size);
+    System.out.print("Your matrix is "+ size);
+    System.out.println(" x "+size);
     System.out.println("");
     System.out.print("Printing matrix with default values:");
+    System.out.println("");
+    matrixes.emptyMatrix(Xsize,Ysize);
+    System.out.println("");
+    System.out.print("Populating matrix. . .matrix populated");
+    System.out.println("");
+    System.out.println("");
+    System.out.print("printing matrix: ");
+    System.out.println("");
+    matrixes.filledMatrix(Xsize, Ysize);
 
 
 
@@ -20,10 +30,6 @@ public class RunMatrix {
 
 
 
-
-
-
-
-
+    scan.close();
   }
 }
